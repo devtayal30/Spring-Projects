e59d75a8bb8e60724c50c08a0e26a011fba01f46
Dev Tayal <devtayal04@gmail.com>
2025-12-05 09:49:16 +0530

    Initial commit

---
+++ src/main/java/com/dev/springboot/myfirstwebapp/login/AuthenticationService.java
@@ -0,0 +1,15 @@
+package com.dev.springboot.myfirstwebapp.login;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+	
+	public boolean authenticate(String username, String password) {
+		
+		boolean isValidUserName = username.equalsIgnoreCase("in28minutes");
+		boolean isValidPassword = password.equalsIgnoreCase("dev123");
+		
+		return isValidUserName && isValidPassword;
+	}
+}
