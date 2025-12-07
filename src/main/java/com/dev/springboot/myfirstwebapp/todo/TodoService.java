796708b0f1bc0e0295b99bd0afa43470092f3dc4
Dev Tayal <devtayal04@gmail.com>
2025-12-07 14:11:12 +0530

    new changes

--- src/main/java/com/dev/springboot/myfirstwebapp/todo/TodoService.java
+++ src/main/java/com/dev/springboot/myfirstwebapp/todo/TodoService.java
@@ -3,25 +3,56 @@ package com.dev.springboot.myfirstwebapp.todo;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.springframework.stereotype.Service;
+
+import jakarta.validation.Valid;
 
 @Service
 public class TodoService {
 	
 	private static List<Todo> todos = new ArrayList<>();
 	
+	private static int todosCount = 0;
+	
 	static {
-		todos.add(new Todo(1, "in28minutes","Learn AWS", 
+		todos.add(new Todo(++todosCount, "in28minutes","Get AWS Certified1", 
 							LocalDate.now().plusYears(1), false ));
-		todos.add(new Todo(2, "in28minutes","Learn DevOps", 
+		todos.add(new Todo(++todosCount, "in28minutes","Learn DevOps1", 
 				LocalDate.now().plusYears(2), false ));
-		todos.add(new Todo(3, "in28minutes","Learn Full Stack Development", 
+		todos.add(new Todo(++todosCount, "in28minutes","Learn Full Stack Development1", 
 				LocalDate.now().plusYears(3), false ));
 	}
 	
 	public List<Todo> findByUsername(String username){
-		return todos;
+		Predicate<? super Todo> predicate = 
+				todo -> todo.getUsername().equalsIgnoreCase(username);
+		return todos.stream().filter(predicate).toList();
+	}
+	
+	public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
+		Todo todo = new Todo(++todosCount,username,description,targetDate,done);
+		todos.add(todo);
+	}
+	
+	public void deleteByID(int id) {
+		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+		todos.removeIf(predicate);
+	}
+	
+	public Todo findById(int id) {
+		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+		Todo todo = todos.stream().filter(predicate).findFirst().get();
+		return todo;
+	}
+
+	public void updateTodo(@Valid Todo todo) {
+		// TODO Auto-generated method stub
+		deleteByID(todo.getId());
+		todos.add(todo);
+		
+		
 	}
 	
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/listTodos.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/listTodos.jsp
@@ -1,9 +1,31 @@
-<html>
-	<head>
-		<title>List Todos Page</title>
-	</head>
-	<body>
-		<div>Welcome to in28minutes</div>
-		<div>Your Todos are ${todos }</div>
-	</body>
-</html>
+    <%@ include file="common/header.jspf" %>
+    <%@ include file="common/navigation.jspf" %>		
+		<div class="container">
+			<h1>Your Todos</h1>
+			<table class="table">
+				<thead>
+					<tr>
+						
+						<th>Description</th>
+						<th>Target Date</th>
+						<th>Is Done?</th>
+						<th></th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>		
+					<c:forEach items="${todos}" var="todo">
+						<tr>
+							
+							<td>${todo.description}</td>
+							<td>${todo.targetDate}</td>
+							<td>${todo.done}</td>
+							<td> <a href="delete-todo?id=${todo.id}" class="btn btn-warning">DELETE ${todo.id}</a></td>
+							<td> <a href="update-todo?id=${todo.id}" class="btn btn-success">Update</a>   </td>
+						</tr>
+					</c:forEach>
+				</tbody>
+			</table>
+		    <a href="add-todo" class="btn btn-success">Add Todo</a>
+		</div>
+		<%@ include file="common/footer.jspf" %>
